3cd7b50a8474224a7164d9d24895c98babf03d8d
media2285-cmyk <media2285@gmail.com>
2026-03-20 11:34:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 설정
 const CONFIG = {
     // Google Apps Script 웹앱 URL (배포 후 여기에 입력)
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwgN8xHmBpBWmz3z-oAYIrTG5QQJQY5tSt7X26ZS-1pWxtioiDK2xmttxYrJzTqK_pC5Q/exec',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwBE3vr0mHTgi2OSB1SX3VpMSAm5TIKSQ5DldcLEQMif9dO2aoPhuFTScJwryXROPVsnA/exec',
     // 담당자 PIN 번호 (변경 가능)
     ADMIN_PIN: '1234',
 };
